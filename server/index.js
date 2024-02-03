17e61b41f6b5fd019ef736eced8ae5e8400672cb
shivam <shivamconnectshere@gmail.com>
2024-02-03 21:24:36 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5173'],
+		origin: ['https://kodeditor-e97f24pao-absorbinginfos-projects.vercel.app'],
 		credentials: true,
 	}),
 );
