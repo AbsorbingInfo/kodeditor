cc14c6ced74abad75d7be95e92fc54dd8bd852cf
shivam <shivamconnectshere@gmail.com>
2024-02-03 21:36:09 +0530

    cors fix 5

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ dotenv.config();
 
 const port = 8000;
 const app = express();
-app.all('*', function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
-app.use(cors());
+
+app.use(
+	cors({
+		origin: ['https://kodeditor-sigma.vercel.app'],
+		credentials: true,
+	}),
+);
 app.use(express.json());
 app.use(cookieParser());
 
